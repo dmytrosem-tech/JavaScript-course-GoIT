7a4393df7506161aa49a29620828f8eab31ba754
Dmytro Semeruk <deemoon83@gmail.com>
2021-06-15 15:01:52 +0300

    5/7

---
+++ js/theory/module5-proto.js
@@ -0,0 +1,17 @@
+// function User({ name, email }) {
+//   this.name = name;
+//   this.email = email;
+// }
+
+// User.prototype.getEmail = function () {
+//   return this.email;
+// };
+
+// User.prototype.changeEmail = function (email) {
+//   this.email = email;
+// };
+
+// const bob = new User({ name: 'Bob', email: 'bob@mail.com' });
+// console.log(bob.getEmail());
+// bob.changeEmail('bobo@mail.com');
+// console.log(bob.getEmail());
