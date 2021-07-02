b3c4fc324653f29743540f635479ce4a5d248262
Dmytro Semeruk <deemoon83@gmail.com>
2021-07-02 13:25:38 +0300

    задача про Австрию

--- js/theory/module6-theory.js
+++ js/theory/module6-theory.js
@@ -179,3 +179,95 @@
 // findScientistBirthDate('Albert', 'Einstein');
 // console.log(findScientistBirthDate('Albert', 'Einstein'));
 // console.log(findScientistBirthDate('Hanna', 'Hammarström'));
+
+async function getData() {
+  let result = null;
+  let response = await fetch('https://restcountries.eu/rest/v2/all');
+  result = await response.json();
+  return result;
+}
+
+getData().then(data => localStorage.setItem('arr', JSON.stringify(data)));
+
+const arr = JSON.parse(localStorage.getItem('arr'));
+console.log(arr);
+
+// Получить стрыны из региона Europe
+// Получить название самой большой страни в Европе
+// Получить название самой маленькой страни в Европе
+// Получить список стран с которыми граничит Швейцария (Switzerland)
+// Получить процент который занимает  Австрия  в мире
+
+// const REGIONS = {
+//   EUROPE: 'Europe',
+//   ASIA: 'Asia',
+// };
+
+const percentInWorld = (inn, array) => {
+  const onePercentOfWorldArea =
+    array.reduce((acc, country) => acc + country.area, 0) / 100;
+
+  const percInTheWorld =
+    array.find(country => country.name === inn).area / onePercentOfWorldArea;
+
+  return percInTheWorld;
+};
+
+const percentInWorldNew = (countryIn, array) =>
+  (array.find(country => country.name === countryIn).area /
+    array.reduce((acc, country) => acc + country.area, 0)) *
+  100;
+
+console.log(
+  'Вариант с переменными и ретурном:',
+  percentInWorld('Austria', arr),
+);
+console.log(
+  'Вариант без переменных и ретурна:',
+  percentInWorldNew('Austria', arr),
+);
+
+// const findCountryByRegion = (countries, region) =>
+//   countries.filter(country => country.region === region);
+// // .map((country) => country.name);
+
+// const countriesInEurope = findCountryByRegion(arr, REGIONS.EUROPE);
+
+// const findLargestAndSmallestCountries = countries => {
+//   const sortedCouytriesList = [...countries].sort((a, b) => b.area - a.area);
+//   return {
+//     max: sortedCouytriesList[0],
+//     min: sortedCouytriesList[sortedCouytriesList.length - 1],
+//   };
+// };
+
+// console.log(`result `, findCountryByRegion(arr, REGIONS.EUROPE));
+// console.log(
+//   'min and max countries',
+//   findLargestAndSmallestCountries(countriesInEurope),
+// );
+
+// const findCountryByName = (contryName, countries) =>
+//   countries.find(country => country.name === contryName);
+
+// const borders = findCountryByName('Switzerland', arr).borders;
+
+// const bordersCountries = (countries, borders) =>
+//   countries
+//     .filter(country => borders.includes(country.alpha3Code))
+//     .map(country => country.name);
+
+// console.log(`bordersCountries`, bordersCountries(countriesInEurope, borders));
+// function findBorders(countryName, countryArray) {
+//   const countryCode = countryArray.find(
+//     el => el.name === countryName,
+//   ).alpha3Code;
+//   console.log(countryCode);
+//   const result = countryArray
+//     .filter(el => el.borders.includes(countryCode))
+//     .map(el => el.name);
+//   console.log(countryName);
+//   return result;
+// }
+
+// console.log(findBorders('Switzerland', arr));
