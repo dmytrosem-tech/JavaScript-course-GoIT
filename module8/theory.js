1170bfc8aaf4dc40fbd299bb2027fb3a047d369a
Dmytro Semeruk <deemoon83@gmail.com>
2021-07-21 13:58:05 +0300

    Plus new template

--- module8/theory.js
+++ module8/theory.js
@@ -21,23 +21,56 @@
 //   console.log(event.target);
 // }
 
-const nav = document.querySelector('.js-nav');
+// const nav = document.querySelector('.js-nav');
 
-nav.addEventListener('click', onNavClick);
+// nav.addEventListener('click', onNavClick);
 
-function onNavClick(event) {
-  event.preventDefault();
-  const target = event.target;
-  console.log('event Target', target);
-  if (target.nodeName !== 'A') {
-    return;
-  }
-  setActiveLink(target);
-}
-function setActiveLink(nextActiveLink) {
-  const currentActiveLink = nav.querySelector('a.active');
-  if (currentActiveLink) {
-    currentActiveLink.classList.add('active');
-  }
-  nextActiveLink.classList.add('active');
-}
+// function onNavClick(event) {
+//   event.preventDefault();
+//   const target = event.target;
+//   console.log('event Target', target);
+//   if (target.nodeName !== 'A') {
+//     return;
+//   }
+//   setActiveLink(target);
+// }
+// function setActiveLink(nextActiveLink) {
+//   const currentActiveLink = nav.querySelector('a.active');
+//   if (currentActiveLink) {
+//     currentActiveLink.classList.add('active');
+//   }
+//   nextActiveLink.classList.add('active');
+// }
+
+const menuData = {
+  title: 'Eat it createElement, templates rule!',
+  items: ['Handlebars', 'LoDash', 'Pug', 'EJS', 'lit-html'],
+};
+const source = document.querySelector('#menu-template').innerHTML.trim();
+
+// В результате мы видим в консоли то,
+// как в переменную source записан обычный
+// форматированый текст.
+const template = Handlebars.compile(source);
+
+/*
+  Создадим переменную markup, в которую запишем 
+  результат работы функции-шаблона template
+*/
+const markup = template(menuData);
+console.log(markup);
+
+/*
+  Пример выше похож на операцию «поиск-и-замена»: шаблон просто 
+  заменил {{title}} на значение свойства menuData.title. 
+  После чего в результат попал сначала текст <ul>, 
+  потом выполнился {{#each items}}, который последовательно 
+  сгенерировал элементы списка, и затем список был закрыт </ul>.
+*/
+
+/*
+  В js выберем контейнер по id, после чего используя 
+  свойство innerHTML присвоим туда нашу строку.
+*/
+const container = document.querySelector('#menu-container');
+container.innerHTML = markup;
