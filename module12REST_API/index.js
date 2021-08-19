c4bb9a6a0a09a9d5a1efdd3f8ed91d11fd4ce700
Dmytro Semeruk <deemoon83@gmail.com>
2021-08-19 21:07:00 +0300

    12Вова

--- module12REST_API/index.js
+++ module12REST_API/index.js
@@ -1,48 +1,48 @@
-const refs = {
-  searchForm: document.querySelector('.js-search-form'),
-  articlesContainer: document.querySelector('.js-articles-container'),
-  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
-};
+// const refs = {
+//   searchForm: document.querySelector('.js-search-form'),
+//   articlesContainer: document.querySelector('.js-articles-container'),
+//   loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+// };
 
-class NewsApiService {
-  constructor() {
-    this.searchQuery = '';
-  }
+// class NewsApiService {
+//   constructor() {
+//     this.searchQuery = '';
+//   }
 
-  fetchArticles() {
-    const options = {
-      headers: {
-        Authorization: 'ae863941357b4c72b8f9092e1fc69ff4',
-      },
-    };
+//   fetchArticles() {
+//     const options = {
+//       headers: {
+//         Authorization: 'ae863941357b4c72b8f9092e1fc69ff4',
+//       },
+//     };
 
-    const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pagesSize=5&page=1`;
+//     const url = `https://newsapi.org/v2/everything?q=${this.searchQuery}&language=en&pagesSize=5&page=1`;
 
-    fetch(url, options)
-      .then(r => r.json())
-      .then(console.log);
-  }
+//     fetch(url, options)
+//       .then(r => r.json())
+//       .then(console.log);
+//   }
 
-  get query() {
-    return this.searchQuery;
-  }
+//   get query() {
+//     return this.searchQuery;
+//   }
 
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
-}
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }
+// }
 
-const newsApiService = new NewsApiService();
+// const newsApiService = new NewsApiService();
 
-refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+// refs.searchForm.addEventListener('submit', onSearch);
+// refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-function onSearch(e) {
-  e.preventDefault();
-  newsApiService.query = e.currentTarget.elements.query.value;
-  newsApiService.fetchArticles();
-}
+// function onSearch(e) {
+//   e.preventDefault();
+//   newsApiService.query = e.currentTarget.elements.query.value;
+//   newsApiService.fetchArticles();
+// }
 
-function onLoadMore() {
-  newsApiService.fetchArticles();
-}
+// function onLoadMore() {
+//   newsApiService.fetchArticles();
+// }
---
+++ module12REST_API/indexVova.js
@@ -0,0 +1,31 @@
+const refs = {
+  form: document.querySelector('#form'),
+  input: document.querySelector('#search'),
+  container: document.querySelector('.container'),
+};
+
+
+const hendlerSubmit = e => {
+  e.preventDefault();
+  const value = refs.input.value;
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
+    .then(res => res.json())
+    .then(data => renderCollection(data.drinks))
+    .catch(err => console.log(err));
+};
+
+function createItem ({strDrinkThumb, strDrink})  {
+    const article = `
+    <article>
+      <img src='${strDrinkThumb}' alt='${strDrink}'/>
+      <p>${strDrink}<p/> 
+    </article>
+    `
+    refs.container.insertAdjacentHTML('beforeend', article)
+}
+
+function renderCollection (arr) {
+    arr.forEach(el => createItem(el))
+}
+
+refs.form.addEventListener('submit', hendlerSubmit);
