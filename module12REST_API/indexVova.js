73e7ffeff5b073af858c803c3536fb6de4bc3f71
Dmytro Semeruk <deemoon83@gmail.com>
2021-08-20 11:59:58 +0300

    task-git-reps

--- module12REST_API/indexVova.js
+++ module12REST_API/indexVova.js
@@ -1,31 +1,30 @@
-const refs = {
-  form: document.querySelector('#form'),
-  input: document.querySelector('#search'),
-  container: document.querySelector('.container'),
-};
+// const refs = {
+//   form: document.querySelector('#form'),
+//   input: document.querySelector('#search'),
+//   container: document.querySelector('.container'),
+// };
 
+// const hendlerSubmit = e => {
+//   e.preventDefault();
+//   const value = refs.input.value;
+//   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
+//     .then(res => res.json())
+//     .then(data => renderCollection(data.drinks))
+//     .catch(err => console.log(err));
+// };
 
-const hendlerSubmit = e => {
-  e.preventDefault();
-  const value = refs.input.value;
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
-    .then(res => res.json())
-    .then(data => renderCollection(data.drinks))
-    .catch(err => console.log(err));
-};
+// function createItem ({strDrinkThumb, strDrink})  {
+//     const article = `
+//     <article>
+//       <img src='${strDrinkThumb}' alt='${strDrink}'/>
+//       <p>${strDrink}<p/>
+//     </article>
+//     `
+//     refs.container.insertAdjacentHTML('beforeend', article)
+// }
 
-function createItem ({strDrinkThumb, strDrink})  {
-    const article = `
-    <article>
-      <img src='${strDrinkThumb}' alt='${strDrink}'/>
-      <p>${strDrink}<p/> 
-    </article>
-    `
-    refs.container.insertAdjacentHTML('beforeend', article)
-}
+// function renderCollection (arr) {
+//     arr.forEach(el => createItem(el))
+// }
 
-function renderCollection (arr) {
-    arr.forEach(el => createItem(el))
-}
-
-refs.form.addEventListener('submit', hendlerSubmit);
+// refs.form.addEventListener('submit', hendlerSubmit);
